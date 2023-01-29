f1d50f5f48858e32108bfee25e720777811c2dc2
Greg French <gregfrench26@gmail.com>
2023-01-28 23:14:57 -0500

    Added solving for circled letters

--- solvejumble.f95
+++ solvejumble.f95
@@ -33,7 +33,7 @@ recursive subroutine generateAnagram(word, wordSize, curr, currSize, wordList, w
     integer, intent(in) :: wordSize
     character (len=15), intent(out) :: curr
     integer, intent(out) :: currSize
-    character (len=15), intent(out) :: wordList(2000)
+    character (len=15), intent(out) :: wordList(10000)
     integer, intent(out) :: wordListSize
     integer, intent(out) :: chosen(15)
     integer :: i
@@ -56,11 +56,13 @@ recursive subroutine generateAnagram(word, wordSize, curr, currSize, wordList, w
     end do
 end subroutine generateAnagram
 
-subroutine findAnagram(wordList, wordListSize)
+subroutine findAnagram(wordList, wordListSize, wordResList, wordResListSize)
     use lexicon
 
-    character (len=15), intent(in) :: wordList(2000)
+    character (len=15), intent(in) :: wordList(10000)
     integer, intent(in) :: wordListSize
+    character (len=15), intent(out) :: wordResList(2000)
+    integer, intent(out) :: wordResListSize
     character (len=15) :: seen(1000)
     integer :: i
     integer :: wordFound
@@ -74,6 +76,9 @@ subroutine findAnagram(wordList, wordListSize)
 
         if (wordFound.EQ.1) then
             if (wasSeen.EQ.0) then
+                wordResList(wordResListSize+1) = wordList(i)
+                wordResListSize = wordResListSize + 1
+
                 write(*, fmt="(A15)", advance="no") wordList(i)
             end if
         end if
@@ -90,16 +95,24 @@ program solvejumble
 
     integer numJumbledWords
     character (len=15) :: words(100)
-    ! generateAnagram
     character (len=15) :: word
     integer :: wordSize
     character (len=15) :: curr
     integer :: currSize
-    character (len=15) :: wordList(2000)
+    character (len=15) :: wordList(10000)
     integer :: wordListSize
+    character (len=15) :: wordResList(2000)
+    integer :: wordResListSize
     integer :: chosen(15)
     integer :: i
     integer :: j
+    character (len=15) :: str
+    character (len=15) :: jumbledWord
+    integer :: jumbledWordSize
+
+    jumbledWord = ""
+    jumbledWordSize = 0
+    wordResListSize = 0
 
     call buildlexicon()
 
@@ -127,6 +140,43 @@ program solvejumble
         write(*, fmt="(A15)", advance="no") words(i)
 
         call generateAnagram(word, wordSize, curr, currSize, wordList, wordListSize, chosen)
-        call findAnagram(wordList, wordListSize)
+        call findAnagram(wordList, wordListSize, wordResList, wordResListSize)
     end do
+
+    print '(A1)', ' '
+
+    write(*, fmt="(A40)") "Select circled letters from word puzzle:"
+
+    do i = 1, wordResListSize
+        write(*, fmt="(a)", advance="no") trim(wordResList(i))
+        write(*, fmt="(A2)", advance="no") ': '
+        read (*, '(a)') str
+
+        do j = 1, 15
+            if (str(j:j).NE." ") then
+                jumbledWord(jumbledWordSize+1:jumbledWordSize+1) = str(j:j)
+                jumbledWordSize = jumbledWordSize + 1
+            end if
+        end do
+            
+    end do
+    
+    write(*, fmt="(a)", advance="no") "Jumbled word: "
+
+    write(*, fmt="(a)") trim(jumbledWord)
+
+    print '(A1)', ' '
+
+    write(*, fmt="(a)", advance="no") "Solved jumble: "
+
+    word = jumbledWord
+    wordSize = jumbledWordSize
+    currSize = 0
+    wordListSize = 0
+    wordResListSize = 0
+    curr = ''
+
+    call generateAnagram(word, wordSize, curr, currSize, wordList, wordListSize, chosen)
+    call findAnagram(wordList, wordListSize, wordResList, wordResListSize)
+
 end program solvejumble
